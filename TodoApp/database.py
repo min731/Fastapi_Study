7325d42e786229836c14cf7f7b207b25f90b5572
min0731 <ypd04208@gmail.com>
2024-05-20 22:35:43 +0900

    240520_fastapi_alembic

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -5,5 +5,16 @@ from sqlalchemy.ext.declarative import declarative_base
 # pip install sqlAlchemy
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db' # db 위치
 engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={'check_same_thread' : False}) # 하나의 스레드만 허용
+
+# Postgre로 변경
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:1234@localhost/TodoApplicationDatabase' # db 위치
+# connect_args는 sqlite 전용
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# Mysql로 변경
+# SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:1234@127.0.0.1:3306/TodoApplicationDatabase' # db 위치
+# connect_args는 sqlite 전용
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False,bind=engine) # 세션 로컬 인스턴스
 Base = declarative_base() # db 호출
